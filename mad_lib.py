17c06d1a9b6d75ca796630691a5eb6b8475c81c5
Alexis <anbird89@gmail.com>
2016-01-07 14:55:34 -0800

    Madlib game with simplified code.

--- mad_lib.py
+++ mad_lib.py
@@ -5,27 +5,27 @@ madlib = "In the early 1900s, crossword "
 
 plural_noun = input("Please give a plural noun: ")
 
-madlib = madlib + plural_noun + " only appeared in children's books. "
+madlib += plural_noun + " only appeared in children's books. "
 
 adjective = input("Please give an adjective, and if you could capitalize the first letter, that would be nice: ")
 
-madlib = madlib + adjective + " kinkajous first created these marvels. "
+madlib += adjective + " kinkajous first created these marvels. "
 
 #They were often considered (adjective2) in those days. Nonetheless, they were quite popular among (pluralnoun2). Today, (person) is a strong proponent of implimenting such practices in (place)
 
 adjective2 = input("Please give an adjective: ")
 
-madlib = madlib + "They were often considered " + adjective2 + " in those days. "
+madlib += "They were often considered " + adjective2 + " in those days. "
 
 plural_noun2 = input("Please give a plural noun: ")
 
-madlib = madlib + "Nonetheless, they were quite popular among " + plural_noun2 + ". "
+madlib += "Nonetheless, they were quite popular among " + plural_noun2 + ". "
 
 person = input("Please name a person: ")
 
 place = input("Please name a place: ")
 
-madlib = madlib + "Today, " + person + " is a strong proponent of implimenting such practices in " + place + "."
+madlib += "Today, " + person + " is a strong proponent of implimenting such practices in " + place + "."
 
 
 print(madlib)
