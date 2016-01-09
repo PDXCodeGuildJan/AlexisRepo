45061fec483539f7c94c094f909e7d1e05b01f40
Alexis <anbird89@gmail.com>
2016-01-08 16:38:14 -0800

    Experimented with if, elif, and else statements.

--- die.py
+++ die.py
@@ -11,27 +11,48 @@ def die():
 
 def custom_die(low, high): # make function accept two inputs
 	roll = randint(low, high) # make sure to have two inputs in the randint() function
-	print(roll)
+	if roll == low:
+		print(roll, "Critical Fail!")
+	elif low < roll < high:
+		print(roll)
+	else:
+		print(roll, "Critical Hit!")
+	
 
 # how big is the die?
 # how many dice?
 
 def main():
+	print("Welcome to Die Roller. Enter q to exit.")
+
+	entree = ""
+
+	while entree != "q":
 	# ask user how many dice to roll
-	total_rolls = input("How many dice would you like to roll? ")
-	total_rolls = int(total_rolls)
+		entree = input("How many dice would you like to roll? ")
+		if entree == "q":
+			#Exit the program
+			exit()
+		total_rolls = int(entree)
 
 	#find out how big the dice are
-	max_num = input("How many sides are on the dice? ")
-	max_num = int(max_num)
+		entree = input("How many sides are on the dice? ")
+		if entree == "q":
+			exit()
+		max_num = int(entree)
 	# you can also "nest": max_num = int(input("How many sides are on the dice?"))
 
 
 	# roll that many dice
 
-	for number in range(0, total_rolls):
-		custom_die(0, max_num)
+		for number in range(0, total_rolls):
+			custom_die(1, max_num)
+
+	
 
 main()
+
+# long game: Let's modify our main so that it continues asking us what dice to roll until we exit the program.
+# Wrap the core logic of the function in a while loop so that it keeps asking to roll until we exit.
 
 
