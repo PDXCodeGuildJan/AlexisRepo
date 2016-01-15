15ea5ab794c08d3988e74c15a844a30ca2cfac81
Alexis <anbird89@gmail.com>
2016-01-14 16:36:16 -0800

    Algorithm for sorting numbers from smallest to largest - runs but doesn't sort the last number

--- Algorithms/sort.py
+++ Algorithms/sort.py
@@ -23,18 +23,51 @@ def selection_sort(unsorted_list):
 
 	# Given a random list of numbers, sort said list into numerical order from smallest to biggest: 
 
-
-	# take the first number in the unsorted list 
-	first_number = unsorted_list[0] # assigns variable to first item in unsorted_list
-	next_number = unsorted_list[] # can I put a blank index here to denote moving through each index in the list??
-
-	# find the smallest number in the unsorted list
-	if first_number > next_number:
-	# Swap the first number and the smallest number in the unsorted list
-
-	# update unsorted list with the next number as the first number in the list now
-
+	print("Unsorted: ", unsorted_list)
+	# find the start of the unsorted list [loop]
+	first_num = 0  # assigns variable to first index in unsorted_list - will keep track of the index 
+	# through the function based on its assigned value, which is technically just a value, not the index itself,
+	# although it will be used to help denote the index throughout the function
+	
 	# repeat until unsorted list is empty
+	while first_num < len(unsorted_list):
+		# find the smallest number in the unsorted list
+		# make the computer look at every item in the list and find the smallest one
+		currentIndex = first_num # whichever index we are currently at and the vantage point from which we are
+		# comparing the rest of the list
+		currentLowestIndex = first_num # the place for whichever number is the smallest thus far
+		while currentIndex < len(unsorted_list[first_num:]): # loops through whatever remaining list there is
+		# using the slice tool, which will progress based on the first_num (which will change each time via
+			# loop) and go to the end of the list, denoted by not putting anything after the colon
+			if unsorted_list[currentIndex] < unsorted_list[currentLowestIndex]: # specifies the number the index
+			# represents rather than the index itself, so if the number at the current index is less than
+			# current lowest number, then this will happen:
+				currentLowestIndex = currentIndex # whatever the 
+				#currentLowestIndex is becomes the currentIndex
+			currentIndex += 1 #currentIndex is redefined by adding 1 to its current value so that when
+			# it loops back through, it will move through the list to the next index until it goes through
+			# the whole list, which is specified with the initial while loop 
+
+		# Swap the first number and the smallest number in the unsorted list
+		# create temporary variable to store what is being swapped so it doesn't disappear during the swap
+		temp = unsorted_list[currentLowestIndex]
+		unsorted_list[currentLowestIndex] = unsorted_list[first_num]
+		unsorted_list[first_num] = temp
+
+		
+		
+
+		# update unsorted list with the next number as the first number in the list now
+		first_num += 1
+		
+
+	return unsorted_list 
+
+	
+test = [4, 68, 57, 19, 3, 40]
+
+sorted_list = selection_sort(test)
+print("Sorted: ", sorted_list)
 	 
 
 
