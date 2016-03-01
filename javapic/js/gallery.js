cca820fcd4f1833fad05480145dd27f2c59d5ae7
Alexis <anbird89@gmail.com>
2016-02-29 16:14:27 -0800

    Fully functioning images in gallery that load and lightbox upon click and disappear when clicked outside image

--- javapic/js/gallery.js
+++ javapic/js/gallery.js
@@ -16,7 +16,11 @@
 
 // var x = document.getElementById("myText").value;
 
+document.addEventListener("click", test);
 
+function test (event) {
+	console.log(event.target.nodeName);
+} 
 window.onload = displayImages;
 
 function displayImages () {
@@ -42,10 +46,12 @@ function displayImages () {
 }
 
 function lightBox (event) {
-	document.getElementById("image_show").className = "display_img";
-	// add event object, make sure you catch it in this function!
-	console.log(event.target.src);
-	document.getElementById("image_show").firstChild.src=event.target.src;
+
+
+	if (event.target.nodeName === "IMG") {
+		document.getElementById("image_show").className = "display_img";
+		document.getElementById("image_show").firstChild.src=event.target.src;
+	}
 	
 }
 
@@ -55,11 +61,17 @@ console.log(lightBox);
 
 // third function to get rid of lightbox
 function removeLightBox (event) {
+
+	if (event.target.nodeName != "IMG") {
 	document.getElementById("image_show").firstChild.src=event.target.src;
 
 	document.getElementById("image_show").className="display_none";
 
+	}
 }
 
 document.getElementById("image_show").addEventListener("click", removeLightBox);
+
+console.log(removeLightBox);
+
 
