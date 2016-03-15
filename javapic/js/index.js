dba7e034c85f1740ac5f14e25e796f58c9dbd48d
Alexis <anbird89@gmail.com>
2016-03-15 16:31:41 -0700

    worked on index page, function is running but image not changing

--- javapic/js/index.js
+++ javapic/js/index.js
@@ -1,17 +1,25 @@
 // Gain access to all of the Gallery Images
-function accessImages() {
+function changeImage() {
 
-	var jumboImage = document.getElementById("jumbotron");
+	
 	// start out with click event listener to change image in jumbotron just to another single image, even if you have to hard code it. Then, see if you can add another image/go through all the images 
+	var jumboImage = document.getElementById("jumbotron");
 
 	for (i = 1; i <= 60; i++) {
 		if (i < 10) {
 			i = "0" + i;
 		}
-		var image = "<img src=images/pdxcg_" + i + ".jpg>";
+		// jumboImage = "<img src=images/pdxcg_" + i + ".jpg>";
+			
 	}
-	jumboImage = "<img src=images/pdxcg_06.jpg>";
-	
+	// jumboImage = "<img src=images/pdxcg_06.jpg>";
+
+
+	jumboImage.style.backgroundImage = "url('images/pdxcg_' "+ i +" '.jpg')";
+	console.log(changeImage, "function running");
 }
 
-document.addEventListener("click", accessImages);
+document.getElementById("jumbotron").addEventListener("click", changeImage);
+changeImage();
+
+
