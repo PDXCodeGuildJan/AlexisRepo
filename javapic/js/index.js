37c7309c4fbb6590db22a88f8490bc339e1d8f0a
Alexis <anbird89@gmail.com>
2016-03-16 10:46:24 -0700

    jumbotron image now switches upon click but stops once it reaches the last gallery image

--- javapic/js/index.js
+++ javapic/js/index.js
@@ -1,25 +1,22 @@
-// Gain access to all of the Gallery Images
+var i = 1;
+
 function changeImage() {
 
 	
 	// start out with click event listener to change image in jumbotron just to another single image, even if you have to hard code it. Then, see if you can add another image/go through all the images 
 	var jumboImage = document.getElementById("jumbotron");
 
-	for (i = 1; i <= 60; i++) {
-		if (i < 10) {
-			i = "0" + i;
-		}
-		// jumboImage = "<img src=images/pdxcg_" + i + ".jpg>";
-			
+	if (i < 10) {
+		i = "0" + i;
 	}
-	// jumboImage = "<img src=images/pdxcg_06.jpg>";
+	
 
 
-	jumboImage.style.backgroundImage = "url('images/pdxcg_' "+ i +" '.jpg')";
-	console.log(changeImage, "function running");
+	jumboImage.style.backgroundImage = "url('images/pdxcg_"+ i +".jpg')";
+	i++;
 }
 
 document.getElementById("jumbotron").addEventListener("click", changeImage);
-changeImage();
+
 
 
