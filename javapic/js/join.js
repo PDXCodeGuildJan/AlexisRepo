3e8cf4252b9e331937966370c1635e1584039917
Alexis <anbird89@gmail.com>
2016-03-18 16:30:11 -0700

    Worked on disabling browser validate, doesn't currently work

--- javapic/js/join.js
+++ javapic/js/join.js
@@ -1,3 +1,4 @@
+// Function to make sure my javascript document is connected to its equivalent HTML
 function externalJava() {
 
 	console.log("Your JS is connected.");
@@ -6,3 +7,15 @@ function externalJava() {
 externalJava();
 
 // 1. Add form validation that works in all major browsers
+
+// Function that deactivates browser validate
+function deactivateBrowserVal() {
+	// Create variable that references the signup fields from the HTML
+	var signup = document.getElementById("signup");
+
+	// builtin function novalidate and set to true to turn off because default is false
+	signup.novalidate = true;
+}
+
+window.onLoad = deactivateBrowswerVal();
+document.createAttribute("novalidate");
